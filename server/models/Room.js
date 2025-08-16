12d483294e399559705e69bb574a52e29fe3fe54
Lulubam <160643605+Lulubam@users.noreply.github.com>
2025-08-16 10:50:09 +0100

    Update Room.js

--- server/models/Room.js
+++ server/models/Room.js
@@ -1,32 +1,29 @@
-// models/Room.js
-
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
-const roomSchema = new Schema({
-    // A unique, human-readable code for the room (e.g., "G2D1F7")
-    code: {
-        type: String,
-        required: true,
-        unique: true
+const roomSchema = new mongoose.Schema({
+    code: { 
+        type: String, 
+        required: true, 
+        unique: true 
     },
-    // The current state of the game, which will be updated in your game-action handler
-    gameState: {
-        type: Object,
-        required: true
-    },
-    // An array of player IDs or objects that are currently in the room
-    players: [{
-        type: Schema.Types.ObjectId, // Reference the Player model
-        ref: 'Player'
+    players: [{ 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Player' 
     }],
-    // Timestamp for when the room was created
-    createdAt: {
-        type: Date,
-        default: Date.now
+    gameState: { 
+        type: mongoose.Schema.Types.Mixed, 
+        default: {} 
+    },
+    isActive: { 
+        type: Boolean, 
+        default: true 
+    },
+    maxPlayers: { 
+        type: Number, 
+        default: 4 
     }
+}, { 
+    timestamps: true 
 });
 
-// Create and export the Mongoose model
-const Room = mongoose.model('Room', roomSchema);
-module.exports = Room;
+module.exports = mongoose.model('Room', roomSchema);
